b88943e6017265ebab2050b5397d575850dd52fb
Gene Diaz <genediazjr@gmail.com>
2021-03-31 15:26:40 +0800

    Updated initial base codes

--- lib/template/api/start.sh
+++ lib/template/api/start.sh
@@ -3,7 +3,7 @@
 echo "NODE ENV:" $NODE_ENV
 
 # Wait for data-migrations to be completed to be ready
-while ping -c 1 $DATA_MIGRATIONS_HOST &> /dev/null; do sleep 1; done
+# while ping -c 1 $DATA_MIGRATIONS_HOST &> /dev/null; do sleep 1; done
 
 # Run the nodejs app
 if [ $NODE_ENV = "integration" ] || [ $NODE_ENV = "staging" ] || [ $NODE_ENV = "production" ]; then
