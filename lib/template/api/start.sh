e40e70afcd48113e26f625d3103869e52859663b
Kyle Salmon <kyleraysalmon@gmail.com>
2022-07-28 18:02:25 -0700

    task: bring in v1.0.0

--- lib/template/api/start.sh
+++ lib/template/api/start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-echo "NODE ENV:" $NODE_ENV
-
 # Run the nodejs app
-if [ $NODE_ENV = "integration" ] || [ $NODE_ENV = "staging" ] || [ $NODE_ENV = "production" ]; then
+if [ "$NODE_ENV" = "integration" ] || [ "$NODE_ENV" = "staging" ] || [ "$NODE_ENV" = "production" ]; then
     npm start
 else
     npm run dev
